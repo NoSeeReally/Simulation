3f1c48766bdfd82d0c7ea1967dc9ea5798792ac8
Angel Nocerale <22-1-01120@vsu.edu.ph>
2026-04-06 01:05:07 +0800

    Resolved

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ import './App.css'
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Header title="My Simulation" />
       <Footer text="© 2026 My Simulation. All rights reserved." />
-    </div>
+    </>
   );
 }
 
